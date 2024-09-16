fe5f46882c2e7f2694c267442efde663c1ee7222
DeadCodeXO <sketchedout@gmail.com>
2024-09-16 18:34:12 -0300

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celestial.hyperoot.dev",
+  site: "https://myc0sis.github.io",
+  base: 
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
